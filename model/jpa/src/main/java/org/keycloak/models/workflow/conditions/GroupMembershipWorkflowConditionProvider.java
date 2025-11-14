002dd83353d3fab2545b4be96f9921f5a24ab5a6
liam dispa <liam.dispa@skygenesisenterprise.com>
2025-11-14 05:39:16 +0100

    Add model folder

---
+++ model/jpa/src/main/java/org/keycloak/models/workflow/conditions/GroupMembershipWorkflowConditionProvider.java
@@ -0,0 +1,57 @@
+package org.keycloak.models.workflow.conditions;
+
+import org.keycloak.models.GroupModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
+import org.keycloak.models.utils.KeycloakModelUtils;
+import org.keycloak.models.workflow.WorkflowConditionProvider;
+import org.keycloak.models.workflow.WorkflowEvent;
+import org.keycloak.models.workflow.WorkflowInvalidStateException;
+import org.keycloak.models.workflow.ResourceType;
+import org.keycloak.utils.StringUtil;
+
+public class GroupMembershipWorkflowConditionProvider implements WorkflowConditionProvider {
+
+    private final String expectedGroup;
+    private final KeycloakSession session;
+
+    public GroupMembershipWorkflowConditionProvider(KeycloakSession session,String expectedGroup) {
+        this.session = session;
+        this.expectedGroup = expectedGroup;
+    }
+
+    @Override
+    public boolean evaluate(WorkflowEvent event) {
+        if (!ResourceType.USERS.equals(event.getResourceType())) {
+            return false;
+        }
+
+        validate();
+
+        String userId = event.getResourceId();
+        RealmModel realm = session.getContext().getRealm();
+        UserModel user = session.users().getUserById(realm, userId);
+        if (user == null) {
+            return false;
+        }
+
+        GroupModel group = KeycloakModelUtils.findGroupByPath(session, realm, expectedGroup);
+        return user.isMemberOf(group);
+    }
+
+    @Override
+    public void validate() {
+        if (StringUtil.isBlank(this.expectedGroup)) {
+            throw new WorkflowInvalidStateException("Expected group path not set.");
+        }
+        if (KeycloakModelUtils.findGroupByPath(session, session.getContext().getRealm(), expectedGroup) == null) {
+            throw new WorkflowInvalidStateException(String.format("Group with name %s does not exist.", expectedGroup));
+        }
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
---
+++ model/jpa/src/main/java/org/keycloak/models/workflow/conditions/RoleWorkflowConditionProvider.java
@@ -0,0 +1,88 @@
+package org.keycloak.models.workflow.conditions;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.keycloak.models.ClientModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.RoleModel;
+import org.keycloak.models.UserModel;
+import org.keycloak.models.workflow.WorkflowConditionProvider;
+import org.keycloak.models.workflow.WorkflowEvent;
+import org.keycloak.models.workflow.WorkflowInvalidStateException;
+import org.keycloak.models.workflow.ResourceType;
+import org.keycloak.models.utils.RoleUtils;
+import org.keycloak.utils.StringUtil;
+
+public class RoleWorkflowConditionProvider implements WorkflowConditionProvider {
+
+    private final String expectedRole;
+    private final KeycloakSession session;
+
+    public RoleWorkflowConditionProvider(KeycloakSession session, String expectedRole) {
+        this.session = session;
+        this.expectedRole = expectedRole;
+    }
+
+    @Override
+    public boolean evaluate(WorkflowEvent event) {
+        if (!ResourceType.USERS.equals(event.getResourceType())) {
+            return false;
+        }
+
+        validate();
+
+        String userId = event.getResourceId();
+        RealmModel realm = session.getContext().getRealm();
+        UserModel user = session.users().getUserById(realm, userId);
+
+        if (user == null) {
+            return false;
+        }
+
+        Set<RoleModel> roles = user.getRoleMappingsStream().collect(Collectors.toSet());
+        RoleModel role = getRole(expectedRole, realm);
+        return role != null && RoleUtils.hasRole(roles, role);
+    }
+
+    @Override
+    public void validate() throws WorkflowInvalidStateException {
+        if (StringUtil.isBlank(expectedRole)) {
+            throw new WorkflowInvalidStateException("Expected role name not set.");
+        }
+        if (getRole(expectedRole, session.getContext().getRealm()) == null) {
+            throw new WorkflowInvalidStateException(String.format("Role with name %s does not exist.", expectedRole));
+        }
+    }
+
+    private RoleModel getRole(String expectedRole, RealmModel realm) {
+        boolean isClientRole = expectedRole.indexOf('/') != -1;
+
+        if (isClientRole) {
+            String[] parts = expectedRole.split("/");
+
+            if (parts.length != 2) {
+                return null;
+            }
+
+            String clientId = parts[0];
+            String roleName = parts[1];
+
+            ClientModel client = session.clients().getClientByClientId(realm, clientId);
+
+            if (client == null) {
+                return null;
+            }
+
+            return client.getRole(roleName);
+        }
+
+        return realm.getRole(expectedRole);
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
